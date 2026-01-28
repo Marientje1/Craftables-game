7ed6914a964fd7c4d65ff37234fe57acd75203c0
Marientje1 <94905244+Marientje1@users.noreply.github.com>
2026-01-28 11:48:38 +0000

    bug fixes

--- game_files/sketch.js
+++ game_files/sketch.js
@@ -4,23 +4,23 @@ let tileSize = 20;
 let width, height;
 let scale = 0.05;
 let terrainSeed = 0;
+let WIDTH, HEIGHT;
 
 
 function setup() {
-  // Get seed from URL parameters
+  WIDTH = window.innerWidth;
+  HEIGHT = window.innerHeight;
+  canvas = createCanvas(WIDTH, HEIGHT);
+  // fetch seed from url
   let params = new URLSearchParams(window.location.search);
   terrainSeed = params.get('seed') ? parseInt(params.get('seed')) : Math.floor(Math.random() * 1000000);
   
-  // Set seeds for reproducible terrain
-  randomSeed(terrainSeed);
-  noiseSeed(terrainSeed);
+  // set random and noise seeds
   
-  frameRate(15);
-  setSeed();
-  createCanvas(WIDTH, HEIGHT);
   width = canvas.width / tileSize;
   height = canvas.height / tileSize;
   generateTerrain();
+  frameRate(15);
 }
 
 function generateTerrain() { // generate terrain using Perlin noise
@@ -34,12 +34,32 @@ function generateTerrain() { // generate terrain using Perlin noise
 
 }
 
-function preload() {
-  img = loadImage('./background.png');
+function draw() {
+  background(135, 206, 235); // Sky blue
+  noStroke();
+  
+  // Draw terrain
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      if (terrain[x] && terrain[x][y] !== undefined) {
+        let val = terrain[x][y];
+        if (val > 0.6) {
+          fill(100, 100, 100); // Stone
+        } else if (val > 0.3) {
+          fill(139, 90, 43); // Dirt
+        } else {
+          fill(34, 139, 34); // Grass
+        }
+        rect(x * tileSize, y * tileSize, tileSize, tileSize);
+      }
+    }
+  }
 }
 
-
-
-function draw() {
-  image(img, WIDTH, HEIGHT);
+function windowResized() {
+  WIDTH = window.innerWidth;
+  HEIGHT = window.innerHeight;
+  resizeCanvas(WIDTH, HEIGHT);
+  width = canvas.width / tileSize;
+  height = canvas.height / tileSize;
 }
